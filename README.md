f6798d5ebd90ae7f3e4fb25018971a421073163f
Lavender Perry <lavender.perry@outlook.com>
2025-07-03 18:39:59 -0700

    Make cross compiler and add root files

--- README.md
+++ README.md
@@ -4,29 +4,10 @@ The operating system for the Foxglove handheld video game system.
 
 Uses the Linux kernel, made for Raspberry Pi.
 
-## Cloning
-
-This repository uses git submodules,
-so you will have to obtain the submodules before using it.
-The commands to do so look something like this:
-```sh
-git clone https://codeberg.org/elle/foxglove-os.git
-# or git clone https://github.com/LavenderPerry/foxglove-os.git
-
-cd foxglove-os
-
-git submodule update --init --recursive
-# some submodules are quite large,
-# so you may specify --depth=1 (or another number) to download less
-```
-
 ## Building
 
-Currently, the only thing working so far is compiling the kernel.
-
-I haven't even established a proper build system,
-but if you really want to build this, inspect `build.sh`
-(but don't just blindly run it!).
+Very work in progress still, but if you really want to build this,
+inspect `build.sh` (but don't just blindly run it!).
 
 ## Running
 
@@ -37,18 +18,12 @@ so use a virtual machine that emulates it (or real hardware if you want).
 
 ## Files
 
-* `linux/`: Raspberry Pi's fork of the Linux kernel source code
-(as a git submodule).
-
+* `build.sh`: The build script, outputs an image and rootfs.
+* `root/`: All the files that are used unmodified in the final image/rootfs.
 * `linux-.config.patch`: The difference between the Linux kernel's default
 generated build configuration and Foxglove's kernel build configuration.
 It is very small right now, but will grow as development continues.
 
-* `build.sh`: An attempt at a build system.
-It is meant to serve as documentation until I implement something more robust.
-
 ## License
 
 This repository is licensed under the zlib license, viewable at `LICENSE.txt`.
-Submodules included within this repository have their own licenses,
-usually viewable at the root of their repositories/directories.
--- build.sh
+++ build.sh
@@ -1,186 +1,127 @@
 #!/bin/sh -e
 
-#
-# Build script for Foxglove OS
-# For now just tries to do all the commands I ran to get it working,
-# and serves as documentation
-# as well as a recovery process in case I somehow lose all my progress.
-# Eventually I might have a proper Makefile or something for all this,
-# or use a tool like buildroot or Yocto.
-#
+######################################
+# Foxglove OS build script           #
+# Builds the entire operating system #
+######################################
 
-#
-# 0. Make sure the person running this script knows what they are doing...
-#
-
-# Big scary warning
-printf "\e[1m\e[31m!! PROBABLY DON'T RUN THIS SCRIPT !!\e[0m\n"
-cat << ENDWARNING
-
-It's more documentation of what I did than an actual working build automation.
-
-The only scenario where you should be using this is if you are me
-and you somehow lost all your progress and want to restore it quickly.
-
-The intended use of this script is to read through it to understand the process.
-
-This script also does not respect anything about your environment.
-There is no configuration, your environment variables may be overwritten, etc.
-It will at least try to restore things to how they were before running though.
-
-The script also assumes all git submodules are properly initialized,
-and your working directory is the root of this repository,
-so make sure both of those are true before running this script.
-
-I cannot make any guarantees that this will work at all,
-or that it's even safe to run on your computer.
-
-ENDWARNING
-
-# Ask for consent
+echo "This script does a lot! You should read through it before running it."
 printf 'Type "yes" to run the script anyways, anything else to exit: '
 read -r input
 [ "$input" = "yes" ]
 
-# Ask again (this one might be a bit annoying...)
-printf "\e[3mAre you SURE?\e[0m: "
-read -r input
-[ "$input" = "yes" ]
-
-# Exit instructions
 echo "Press Ctrl+C at any time to stop this script."
 sleep 3
 
 #
-# 1. Setup environment
+# Set all environment variables for the rest of the build process.
+# Feel free to modify these.
+# If you don't intend to modify the directory variables,
+# run this from the repo root.
 #
 
-# Script-wide variables
+arch=arm
+ccarch=${arch}v6kz
+target=$ccarch-linux-musleabihf
 privesc=sudo
+threads=$(nproc)
 outname=foxglove-$(date -u +%Y%m%d)
 repodir=$PWD
+rootdir=$repodir/root
 buildir=$repodir/build
-rootdir=$buildir/$outname
-bootdir=$rootdir/boot
-
-# Make directories for rootfs
-mkdir -p "$bootdir"
-
-# Set the number of threads make will use.
-#
-# Here, it is set to the amount of CPU cores the machine has.
-# You could also try modifying this by adding 1, subtracting 2,
-# or multiplying by 1.5 (round down to nearest whole number).
-# There's not really a consensus on what gives the best performance,
-# other than if you have multiple cores, it should be greater than 1.
-#
-# However, some make tasks may not be designed to run concurrently.
-# In this script, I may override the value set here in those cases,
-# but if a make task isn't executing things in the right order,
-# try running it with jobs set to 1.
-prev_MAKEFLAGS=$MAKEFLAGS
-MAKEFLAGS="--jobs=$(nproc)"
-export MAKEFLAGS
+srcsdir=$buildir/sources
+crosdir=$buildir/toolchain
+prevpath=$PATH
+PATH=$crosdir/bin:$PATH
 
 #
-# 2. Build and install the Linux kernel
+# Get the sources that are involved in the operating system,
+# including the kernel, build tools, etc.
+# Also get the firmware binaries.
 #
+
+ghget() {
+    base="${1##*/}"
+    name="$base-${2##*/}"
+    curl -o "$base.tar.gz" "https://codeload.github.com/$1/tar.gz/$2"
+    echo "$name"
+}
+
+mkdir -p "$srcsdir"
+cd "$srcsdir"
+
+# Source code
+mussel="$(ghget firasuke/mussel 95dec40aee2077aa703b7abc7372ba4d34abb889)"
+linux="$(ghget raspberrypi/linux refs/tags/stable_20250428)"
+musseldir="$srcsdir/$mussel"
+linuxdir="$srcsdir/$linux"
+
+# Proprietary firmware binaries
+repo="raspberrypi/firmware"
+commit="effea745e592ec8a97fc54093a5673a7e6e515c9"
+for bin in bootcode.bin fixup.dat start.elf; do
+    curl -o $bin https://raw.githubusercontent.com/$repo/$commit/boot/$bin
+done
+
+# Check file validity (exits if any files are invalid)
+sha256sum -c "$repodir/sources.sha256"
+
+# Extract sources
+for src in *.tar.gz; do tar xf "$src"; done
+
+#
+# Make the cross compiler for building the rest of the operating system.
+# Currently, it uses the mussel project.
+# Eventually, I may write my own script based on mussel if needed.
+#
+
+cd "$buildir"
+
+"$musseldir/check"
+"$musseldir/mussel" "$ccarch" -l -p
+
+#
+# Build the Linux kernel.
 # Resources:
-# https://www.raspberrypi.com/documentation/computers/linux_kernel.html
-# https://www.kernel.org/doc/html/latest/admin-guide/README.html
+#     https://www.raspberrypi.com/documentation/computers/linux_kernel.html
+#     https://www.kernel.org/doc/html/latest/admin-guide/README.html
 #
 
-cd linux
+buildmk() { make -j"$threads" ARCH="$arch" CROSS_COMPILE="$target-" "$@"; }
 
-# Save original values of the variables exported here, to restore later
-prev_ARCH=$ARCH
-prev_CROSS_COMPILE=$CROSS_COMPILE
-
-# Define variables used in the build
-# Probably change these if something goes wrong here, they are specific to a:
-#
-# cross compiled kernel build
-#
-# for the ARM EABI
-# (intended to run on ARMv6Z, the Raspberry Pi Zero W, the Foxglove hardware)
-#
-# on an x86_64 Arch Linux machine
-# (yes "i use arch btw" but it's actually relevant to the CROSS_COMPILE variable
-#  because I'm using the toolchain from the Arch repos)
-configtask=bcmrpi_defconfig
-kernel=kernel
-image=zImage
-export ARCH=arm
-export CROSS_COMPILE=arm-none-eabi-
+cd "$linuxdir"
 
 # Clean the source tree.
 # Might not be needed, but still recommended before every kernel build
 make mrproper
 
 # Configure the build
-make $configtask
-patch .config < ../linux-.config.patch
+buildmk bcmrpi_defconfig
+patch .config < "$repodir"/linux-.config.patch
 
-# Compile the kernel (this one takes a while...)
-make $image modules dtbs
-
-# Install the kernel
-make "INSTALL_MOD_PATH=$rootdir" modules_install
-cp arch/$ARCH/boot/$image "$bootdir/$kernel.img"
-cp arch/$ARCH/boot/dts/broadcom/*.dtb "$bootdir"
-cp arch/$ARCH/boot/dts/overlays/*.dtb* "$bootdir/overlays"
-cp arch/$ARCH/boot/dts/overlays/README "$bootdir/overlays"
+# Compile the kernel (takes a while...)
+buildmk zImage modules dtbs
 
 #
-# 3. Add proprietary firmware
-#
-# At some point I could try unofficial open source firmware but for now,
-# download the binaries of the official firmware
-#
-
-cd "$bootdir"
-
-# Download the firmware binaries
-repo="raspberrypi/firmware"
-commit="effea745e592ec8a97fc54093a5673a7e6e515c9"
-for binary in bootcode.bin fixup.dat start.elf; do
-    curl https://raw.githubusercontent.com/$repo/$commit/boot/$binary -o $binary
-done
-
-# Verify the downloaded files, exits on failure
-sha256sum -c "$repodir/proprietary-firmware-hashes.sha256"
-
-#
-# 4. Create compressed rootfs and disk image
-#
-# Much of this part uses Void Linux's mkimage and mkrootfs scripts as reference:
-# https://github.com/void-linux/void-mklive/blob/906652a/mkimage.sh
-# https://github.com/void-linux/void-mklive/blob/906652a/mkrootfs.sh
+# Make the disk image for the operating system files to be installed to.
+# Much of this uses Void Linux's live image scripts for reference:
+#     https://github.com/void-linux/void-mklive
 #
 
 cd "$buildir"
 
-# Create the compressed rootfs
-tarfile="$outname.tar.xz"
-tar cp --posix \
-    --group=root --owner=root \
-    --xattrs --xattrs-include='*' \
-    -C "$rootdir" . | xz -T0 -9 > "$tarfile"
-
 # Sizes in disk sectors
-reserve=2048
-bootsize=32768 # 16mb, but may want to increase later... recommended is 256mb
-# rootsize is set to the size of all files that will be on root partition
-read -r rootsize _ <<ENDCMD
-$(du -B 512 -s --exclude=boot "$rootdir")
-ENDCMD
-fullsize=$((reserve + bootsize + rootsize))
+reserve=2048 # 1mb
+bootsize=524288 # 256mb
+rootsize=1570816 # 767mb
+fullsize=$((reserve + bootsize + rootsize)) # 1gb
 
 # Create the image file
 imagefile=$outname.img
 truncate -s $((fullsize * 512)) "$imagefile"
 
 # Create the partitions
+# The first one is for boot files, the second is for the rest of the OS
 sfdisk "$imagefile" <<ENDINPUT
 label: dos
 start=$reserve, size=$bootsize, type=b, bootable
@@ -198,34 +139,50 @@ $privesc mkfs.ext4 -O ^has_journal "$p2"
 
 # Create a temporary directory for mounting the partitions
 imagedir=$(mktemp -d)
-imageboot=$imagedir/boot
+imageboot="$imagedir/boot"
 
 # Mount the image partitions
 $privesc mount "$p2" "$imagedir"
 $privesc mkdir -p "$imageboot"
 $privesc mount "$p1" "$imageboot"
 
-# Extract the compressed rootfs onto the image
-$privesc tar xfp "$tarfile" --xattrs --xattrs-include='*' -C "$imagedir"
+# Copy all the root files over
+$privesc cp -r "$rootdir" "$imagedir"
+
+# Add the Linux kernel files
+$privesc make -C "$linuxdir" "INSTALL_MOD_PATH=$imagedir" modules_install
+$privesc cp "$linuxdir"/arch/arm/boot/zImage "$imageboot"
+$privesc cp "$linuxdir"/arch/arm/boot/dts/broadcom/*.dtb "$imageboot"
+$privesc cp "$linuxdir"/arch/arm/boot/dts/overlays/*.dtb* "$imageboot/overlays"
+$privesc cp "$linuxdir"/arch/arm/boot/dts/overlays/README "$imageboot/overlays"
+
+# Copy the firmware
+$privesc cp "$srcsdir/bootcode.bin" "$imageboot"
+$privesc cp "$srcsdir/fixup.dat" "$imageboot"
+$privesc cp "$srcsdir/start.elf" "$imageboot"
+
+# Create a tarfile of the rootfs
+tarfile="$outname.tar"
+tar cp --posix --group=root --owner=root --xattrs --xattrs-include='*' \
+    --file "$tarfile" "$imagedir"
 
 # Unmount image partitions and deconfigure loop device
 $privesc umount -R "$imagedir"
 $privesc losetup -d "$loopdev"
 rmdir "$imagedir" || echo "WARNING: $imagedir not empty after unmount"
 
-# Compress the image
-xz -T0 -9 "$imagefile"
+# Compress the tarfile and imagefile
+xz -T0 -9 "$tarfile" "$imagefile"
+
+cat <<EOF
+Outputs:
+    $buildir/$imagefile.xz
+    $buildir/$tarfile.xz
+EOF
 
 #
-# 5. Cleanup
+# Restore previous environment
 #
 
+PATH=$prevpath
 cd "$repodir"
-
-# Restore environment to whatever it was before this script
-ARCH=$prev_ARCH
-export ARCH
-CROSS_COMPILE=$prev_CROSS_COMPILE
-export CROSS_COMPILE
-MAKEFLAGS=$prev_MAKEFLAGS
-export MAKEFLAGS
