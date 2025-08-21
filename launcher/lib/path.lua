37ebb30a208f8706fa2ec786610c06cdb372f75a
Lavender Perry <lavender.perry@outlook.com>
2025-08-20 17:55:55 -0700

    Add to path utils

--- launcher/lib/path.lua
+++ launcher/lib/path.lua
@@ -13,7 +13,15 @@ function path.join(...)
     return table.concat({ ... }, "/")
 end
 
+--- Constructs a path within the save directory
+--- @param ... string The components of the path
+--- @return string # The resulting path
+function path.full(...)
+    return path.join(path.saveDirectory, ...)
+end
+
 --- Checks if a file/directory/etc exists at the given path
+--- This function is kind of useless and may be removed soon
 --- @param p string The path to check
 --- @return boolean # If the path exists
 function path.exists(p)
